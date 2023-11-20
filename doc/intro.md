1e03d4e423d07b337c3cf6baf84bca60ad57c745
Josh Hartley <jhartley@jhartley.net>
2023-11-20 23:28:19 +0000

    init w/ neil

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to practice-log/practice-log
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/practice_log/practice_log_test.clj
@@ -0,0 +1,7 @@
+(ns practice-log.practice-log-test
+  (:require [clojure.test :refer :all]
+            [practice-log.practice-log :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
