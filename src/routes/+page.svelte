dd3fceea4fb09c10250c4ef273ed31aed79cca1e
Josh Hartley <me@jhartley.net>
2024-01-12 19:48:14 -0500

    add tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<nav>
+    <h1 class="text-3xl">PracticeLog</h1>
+</nav>
+
+<input />
---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+const daisyui = require('daisyui');
+const typography = require('@tailwindcss/typography');
+const forms = require('@tailwindcss/forms');
+
+/** @type {import('tailwindcss').Config}*/
+const config = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+
+	theme: {
+		extend: {}
+	},
+
+	plugins: [forms, typography, daisyui],
+
+	daisyui: {
+		themes: ["nord"]
+	}
+};
+
+module.exports = config;
